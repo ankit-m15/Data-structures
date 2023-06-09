daab542b9d933cd35aa9c0c5879e9e4d102e5bed
Ankit Maurya <85441275+ankit-m15@users.noreply.github.com>
2023-06-09 16:59:02 +0530

    Update all_sorting.cpp

--- sorting/all_sorting.cpp
+++ sorting/all_sorting.cpp
@@ -15,7 +15,7 @@ void bubble_sort(int arr[],int n)
 {
     for(int i=0; i<n; i++)
     {
-        for(int j=1; j<n; j++)
+        for(int j=i; j<n; j++)
         {
             if(arr[j-1]>arr[j]) 
             {
